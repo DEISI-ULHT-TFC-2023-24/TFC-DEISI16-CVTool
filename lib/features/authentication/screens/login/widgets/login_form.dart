db2027cbf9a5a1ea381270b6a63bcada70b3b8f2
DIONI <dionisiomiron@gmail.com>
2024-04-14 02:05:09 +0100

    Business Offers working

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -42,7 +42,7 @@ class _LoginFormState extends State<LoginForm> {
               },
               decoration: const InputDecoration(
                   prefixIcon: Icon(Iconsax.direct_right),
-                  labelText: TFCTexts.username),
+                  labelText: TFCTexts.username)
             ),
             const SizedBox(height: TFCSizes.spaceBtwInputFields),
 
--- lib/users/business/widgets/offers/business_ofertas.dart
+++ lib/users/business/widgets/offers/business_ofertas.dart
@@ -1,11 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import '../../../private/models/offer_model.dart';
 import '../../models/business_user_model.dart';
 
 class BusinessOffersScreen extends StatefulWidget {
-  const BusinessOffersScreen({super.key, required this.user});
+  const BusinessOffersScreen({Key? key, required this.user}) : super(key: key);
 
   final BusinessUser user;
 
@@ -14,27 +12,27 @@ class BusinessOffersScreen extends StatefulWidget {
 }
 
 class _OffersPageState extends State<BusinessOffersScreen> {
-  // Dummy List
-
-  late BusinessUser user;
-
-  late List<OfferModel> offers = user.offers;
-
-  void updateList(String value) {
-    // This function will do the filter in our list.
-    setState(() {
-      offers = user.offers
-          .where((element) =>
-              element.job!.toLowerCase().contains(value.toLowerCase()))
-          .toList();
-    });
-  }
+  late List<OfferModel> offers;
 
   @override
   void initState() {
     super.initState();
-    user = widget.user;
-    offers = user.offers;
+    offers = widget.user.offers;
+  }
+
+  void updateList(String value) {
+    setState(() {
+      offers = widget.user.offers
+          .where((element) =>
+          element.job!.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
+  void _addOffer(OfferModel newOffer) {
+    setState(() {
+      offers.add(newOffer);
+    });
   }
 
   @override
@@ -64,7 +62,9 @@ class _OffersPageState extends State<BusinessOffersScreen> {
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewOfferScreen(_addOffer)));
+                  },
                   child: Container(
                     padding: const EdgeInsets.all(5),
                     child: const Icon(
@@ -100,85 +100,165 @@ class _OffersPageState extends State<BusinessOffersScreen> {
             Expanded(
               child: offers.isEmpty
                   ? const Center(
-                      child: Text(
-                        "Sem resultados",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    )
+                child: Text(
+                  "Sem resultados",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
                   : Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.grey[300],
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.grey[300],
+                ),
+                padding: const EdgeInsets.all(15),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: offers.length,
+                  itemBuilder: (context, index) => ListTile(
+                    contentPadding: const EdgeInsets.all(8.0),
+                    title: Text(
+                      offers[index].job!,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
                       ),
-                      padding: const EdgeInsets.all(15),
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: offers.length,
-                        itemBuilder: (context, index) => ListTile(
-                          contentPadding: const EdgeInsets.all(8.0),
-                          title: Text(
-                            offers[index].job!,
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          offers[index].company!,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          offers[index].local!,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Row(children: [
+                          const Text(
+                            'Vagas: ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                          Text(
+                            offers[index].nrOffers!,
                             style: const TextStyle(
                               color: Colors.black,
                               fontWeight: FontWeight.bold,
-                              fontSize: 16,
+                              fontSize: 14,
                             ),
                           ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                offers[index].company!,
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              Text(
-                                offers[index].local!,
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              Row(children: [
-                                const Text(
-                                  'Vagas: ',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                Text(
-                                  offers[index].nrOffers!,
-                                  style: const TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ]),
-                            ],
-                          ),
-                          trailing: TextButton(
-                            onPressed: () {},
-                            child: const Icon(
-                              Icons.arrow_forward_ios,
-                            ),
-                          ),
-                          leading: const Icon(
-                            Icons.work,
-                            color: Colors.black,
-                          ),
-                        ),
+                        ]),
+                      ],
+                    ),
+                    trailing: TextButton(
+                      onPressed: () {},
+                      child: const Icon(
+                        Icons.arrow_forward_ios,
                       ),
                     ),
+                    leading: const Icon(
+                      Icons.work,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewOfferScreen extends StatefulWidget {
+  final Function(OfferModel) addOffer;
+
+  const NewOfferScreen(this.addOffer);
+
+  @override
+  _NewOfferScreenState createState() => _NewOfferScreenState();
+}
+
+class _NewOfferScreenState extends State<NewOfferScreen> {
+  late String _job;
+  late String _company;
+  late String _local;
+  late String _nrOffers;
+
+  @override
+  void initState() {
+    super.initState();
+    _job = '';
+    _company = '';
+    _local = '';
+    _nrOffers = '';
+  }
+
+  void _submitOffer() {
+    final newOffer = OfferModel(
+      job: _job,
+      company: _company,
+      local: _local,
+      nrOffers: _nrOffers,
+      creationDate: DateTime.now(),
+    );
+    OfferModel.main_offers_list.add(newOffer);
+    widget.addOffer(newOffer);
+    Navigator.pop(context);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Offer'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              onChanged: (value) => setState(() => _job = value),
+              decoration: InputDecoration(labelText: 'Job'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              onChanged: (value) => setState(() => _company = value),
+              decoration: InputDecoration(labelText: 'Company'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              onChanged: (value) => setState(() => _local = value),
+              decoration: InputDecoration(labelText: 'Local'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              onChanged: (value) => setState(() => _nrOffers = value),
+              decoration: InputDecoration(labelText: 'Number of Offers'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _submitOffer,
+              child: const Text('Add Offer'),
             ),
           ],
         ),
